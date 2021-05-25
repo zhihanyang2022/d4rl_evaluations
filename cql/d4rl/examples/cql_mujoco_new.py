3999a55e47143f2a9504f5fed380eb9362e4392a
Zhihan Yang <yangz2@carleton.edu>
2021-05-25 10:06:58 +0800

    minor fix

--- cql/d4rl/examples/cql_mujoco_new.py
+++ cql/d4rl/examples/cql_mujoco_new.py
@@ -307,7 +307,7 @@ if __name__ == "__main__":
     variant['replay_buffer_size'] = mhf.get_dataset_size(args.env)
 
     # added/modified by Zhihan: use 1M grad steps, report avg return across 10 episodes per 10K grad steps
-    variant['algorithm_kwargs']['num_epochs'] = 1
+    variant['algorithm_kwargs']['num_epochs'] = 100
     variant['algorithm_kwargs']['num_trains_per_train_loop'] = int(5e3)
     variant['algorithm_kwargs']['num_eval_steps_per_epoch'] = 10 * variant['algorithm_kwargs']['max_path_length']
 
